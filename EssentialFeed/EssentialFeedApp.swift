9d40a3d8e251ab55a73ad438e6e0e24722ec1e0a
Litvak Lev <sn.delui@gmail.com>
2022-07-26 18:20:48 +0500

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Lev Litvak on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
