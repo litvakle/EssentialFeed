04c50de17b06bbb4b5e9fe4d7781c3ba45e71bf7
Litvak Lev <sn.delui@gmail.com>
2022-08-04 11:28:58 +0500

    Deletes cache on retrieval error

--- EssentialFeedMacOSTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedMacOSTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -75,6 +75,15 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
             store.completeRetrieval(with: feed.local, timeStamp: moreThanSevenDaysOldTimeStamp)
         })
     }
+    
+    func test_load_deletesCacheOnRetrievalError() {
+        let (sut, store) = makeSUT()
+
+        sut.load { _ in }
+        store.completeRetrieval(with: anyNSError())
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve, .deleteCachedFeed])
+    }
 
     // MARK: - Helpers
     
