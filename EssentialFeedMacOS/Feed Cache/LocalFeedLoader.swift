3f9add3781499a523e164499e330b02d1c063a7f
Litvak Lev <sn.delui@gmail.com>
2022-08-04 08:11:58 +0500

    Replace Load command completion to return a Result type instead of Error?

--- EssentialFeedMacOS/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeedMacOS/Feed Cache/LocalFeedLoader.swift
@@ -12,6 +12,7 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
     
     public typealias SaveResult = Error?
+    public typealias LoadResult = LoadFeedResult
     
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -30,10 +31,10 @@ public final class LocalFeedLoader {
         }
     }
     
-    public func load(completion: @escaping (Error?) -> Void) {
+    public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { error in
             if let error = error {
-                completion(error)
+                completion(.failure(error))
             }
         }
     }
--- EssentialFeedMacOSTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedMacOSTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -29,8 +29,14 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         let exp = expectation(description: "Wait for load completion")
         var receivedError: Error?
-        sut.load { error in
-            receivedError = error
+        sut.load { result in
+            switch result {
+            case .failure(let error):
+                receivedError = error
+            default:
+                XCTFail("Expected failure, gor \(result) instead")
+            }
+            
             exp.fulfill()
         }
         
