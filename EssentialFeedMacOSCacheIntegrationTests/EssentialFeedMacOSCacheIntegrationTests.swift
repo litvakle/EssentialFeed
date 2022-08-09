bd5a4954eca61eca228b372d00dbb185ddec4239
Litvak Lev <sn.delui@gmail.com>
2022-08-09 15:52:29 +0500

    Add EssentialFeedCacheIntegration target

---
+++ EssentialFeedMacOSCacheIntegrationTests/EssentialFeedMacOSCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedMacOSCacheIntegrationTests.swift
+//  EssentialFeedMacOSCacheIntegrationTests
+//
+//  Created by Lev Litvak on 09.08.2022.
+//
+
+import XCTest
+
+class EssentialFeedMacOSCacheIntegrationTests: XCTestCase {
+
+    
+}
