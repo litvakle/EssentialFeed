b5433273a4ba8da20b6e181737d6ce64f4c2a1b9
Litvak Lev <sn.delui@gmail.com>
2022-08-11 12:54:41 +0500

    Add 'EssentialFeediOS' framework (prod and test) targets for iOS platform specific components

---
+++ EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Lev Litvak on 11.08.2022.
+//
+
+import XCTest
+
+class EssentialFeediOSTests: XCTestCase {
+
+    
+}
