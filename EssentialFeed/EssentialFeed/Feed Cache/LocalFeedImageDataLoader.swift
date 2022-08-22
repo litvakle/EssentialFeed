df24b37e9ab0b7f313ef6fe117374b7d74ab3a84
Litvak Lev <sn.delui@gmail.com>
2022-08-22 18:29:14 +0500

    'LocalFeedImageDataLoader' does not message store upon creation

---
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -0,0 +1,17 @@
+//
+//  LocalFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Lev Litvak on 22.08.2022.
+//
+
+import Foundation
+
+public class LocalFeedImageDataLoader {
+    public init(store: Any) {
+        
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+    }
+}
