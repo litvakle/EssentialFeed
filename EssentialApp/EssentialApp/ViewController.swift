eda262265c2baa536f346e5b604885853583e305
Litvak Lev <sn.delui@gmail.com>
2022-08-24 14:34:01 +0500

    Add empty 'EssentialApp' project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Lev Litvak on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
